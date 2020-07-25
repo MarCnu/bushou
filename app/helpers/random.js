9adea2ff6020ad3dbaf1775f700db748122ff7e3
MarCnu <marceau.cnudde@gmail.com>
2020-07-25 18:39:35 +0200

    Non uniform randomness for selecting words
    Changes the random distribution of the word selection. Instead of a uniform distribution, the first 50% of words and the most recent 10% will be selected both 25% of the time instead of 50% and 10%.

--- app/helpers/random.js
+++ app/helpers/random.js
@@ -5,7 +5,7 @@ export const getRandomProperty = (obj, answer = false) => {
   // If we need to make an excepetion for one answer
   if(answer !== false) keys.splice(keys.indexOf(answer), 1)
   
-  let index = keys.length * Math.random() << 0
+  let index = getNonUniformRandomIndex(keys.length)
   let response = obj[keys[index]]
 
   // Add the caracter (the key in the object) in the response
@@ -16,7 +16,24 @@ export const getRandomProperty = (obj, answer = false) => {
 
 export const getRandomIndex = (obj) => {
   let keys = Object.keys(obj)
-  return keys[keys.length * Math.random() << 0]
+  return keys[getNonUniformRandomIndex(keys.length)]
+}
+
+/**
+ * Produces a non uniform random distribution to overrepresent the most recent words
+ * Uniform: 0 to 99. Non uniform: 0 to 194. 195 = 50 + 25*2 + 15*3 + 10*5
+ * The first 50% and last 10% words will be selected 25.6% of the times (50/195)
+ */
+export const getNonUniformRandomIndex = (length) => {
+  // For short word lists, we keep the uniform distribution
+  if(length < 40) return length * Math.random() << 0
+  
+  let uniformRand = Math.random() * 1.95
+  let rand = uniformRand - 0.95
+  if(uniformRand < 1.45) rand = uniformRand - 0.55
+  if(uniformRand < 1.0) rand = uniformRand - 0.25
+  if(uniformRand < 0.5) rand = uniformRand
+  return length * rand << 0
 }
 
 /**
